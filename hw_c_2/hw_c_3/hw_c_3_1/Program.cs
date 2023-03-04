bc47c9cbb2f1dbc6a9d148066183c14aa59fe2b1
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-04 08:52:46 +0300

    Complite 1 prog

--- hw_c_2/hw_c_3/hw_c_3_1/Program.cs
+++ hw_c_2/hw_c_3/hw_c_3_1/Program.cs
@@ -1,3 +1,24 @@
 ﻿//Напишите программу, которая принимает на 
 //вход пятизначное число и проверяет, является ли оно палиндромом.
 
+void polindrom(int num){
+    int first = (int)(num / Math.Pow(10, (int)Math.Log10(num)));
+    int last = num%10;
+
+    int two_two = num%10000;
+    int two = (int)(two_two / Math.Pow(10, (int)Math.Log10(two_two)));
+
+    int three_three = num%1000;
+    int three = (int)(three_three / Math.Pow(10, (int)Math.Log10(three_three)));
+
+    int four_four = num%100;
+    int four = (int)(four_four / Math.Pow(10, (int)Math.Log10(four_four)));
+
+    if (first == last && two == four){
+        Console.WriteLine("Полиндром!");
+    }else{
+        Console.WriteLine("Не полиндром!");
+    }
+}
+
+polindrom(12321);
