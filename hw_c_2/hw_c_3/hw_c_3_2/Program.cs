e3621de7c66f2381ce14970c3bc7e9629b1da1c8
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-04 08:52:58 +0300

    Complite 2 prog

---
+++ hw_c_2/hw_c_3/hw_c_3_2/Program.cs
@@ -0,0 +1,7 @@
+﻿double dis(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2)+ Math.Pow(z2 - z1,2));
+}
+
+
+Console.WriteLine(dis(3, 6, 2, 1, 6, 8));
