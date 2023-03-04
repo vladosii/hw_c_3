b28b14a4bd924e0d174bd2b80f6a4d075ccda376
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-04 08:53:08 +0300

    Complite 3 prog

---
+++ hw_c_2/hw_c_3/hw_c_3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 23. Напишите программу, которая 
+//принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N
+
+void square(double num){
+    for (int i = 1; i <= num; i++)
+    {
+        double square = Math.Pow(i,3);
+        Console.WriteLine(square);
+    }
+}
+
+square(5);
