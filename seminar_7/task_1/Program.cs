c857bad75320abbc4d49608c7d4bbf525a286b39
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-16 10:41:13 +0300

    Created task 1

--- seminar_7/task_1/Program.cs
+++ seminar_7/task_1/Program.cs
@@ -1,6 +1,34 @@
-﻿
+﻿// 0. Задайте двумерный массив размером m×n, 
+//    заполненный случайными целыми числами. 
 
-void massive(int[,] mass){
 
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
 }
 
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
---
+++ seminar_7/task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размера m на n, каждый
+// элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+
+void mas(int n, int m)
+{
+    int[,] massiv = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int massiv = new Random().Next(1, 100);
+            mas[i,j] = num;
+            Console.WriteLine($"mas[i,j]");
+        }
+    }
+}
+
+mas(3,3);
