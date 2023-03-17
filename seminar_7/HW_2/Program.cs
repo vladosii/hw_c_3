1784d012dce9fe22bd8b02413f9ce95275b7dae0
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-17 08:29:10 +0300

    Comlite HW2

---
+++ seminar_7/HW_2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void PrintElements(int[,] arr, int posN, int posM)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    if (posN > row || posM > column){
+        Console.WriteLine("Err: EOF");
+    }else{
+        Console.WriteLine($"Elements: {arr[posN-1, posM-1]}");
+    }
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int posN = int.Parse(Console.ReadLine()!);
+int posM = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
+PrintElements(mass, posN, posM);
