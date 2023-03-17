a4f84ac7d6acfb93c83fe4edca0ac2c1b86f198d
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-17 08:18:03 +0300

    Complite HW1

---
+++ seminar_7/HW_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+
+void Print(double[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to)/10.0;
+    return arr;
+}
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
