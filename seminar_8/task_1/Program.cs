edb2ab0a5a1cf4c6a8cf0519dbdd97235ebad66b
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-20 09:55:25 +0300

    Created file Seminar 8

---
+++ seminar_8/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// double[,] fillMass(int n, int m){
+//     double[,] arr = new double[n, m];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             arr[i,j] = new Random().Next(1, 100)/10.0;
+//             System.Console.Write($" {arr[i, j]} ");
+//         }
+//         System.Console.WriteLine(" ");
+//     }
+
+//     return arr;
+// }
+
+// fillMass(3, 3);
